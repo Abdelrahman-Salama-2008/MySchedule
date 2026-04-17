62585c53b7b5a50ad0f597b95c30780eb726a609
Abdelrahman Salama <abdelrahmangharib17@gmail.com>
2026-04-17 23:13:29 +0300

    complete explorer navigation and added icon in cards

---
+++ app/src/main/java/com/example/myschedule/ExplorerActivity.java
@@ -0,0 +1,132 @@
+package com.example.myschedule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity; // 1. Added missing import
+
+import org.w3c.dom.Text;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+// 2. Fixed "A" and "C" in AppCompatActivity
+public class ExplorerActivity extends AppCompatActivity
+{
+    ArrayList<Lecture> lectures = new ArrayList<>();
+    LinearLayout container;
+    TextView dateHeader;
+    Button prevButton;
+    Button nextButton;
+    TextView dayName;
+
+    public String days[] = new String[]{"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+    public int index = 0;
+    DayOfWeek today = LocalDate.now().getDayOfWeek();
+
+    DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("hh:mm a");
+    DateTimeFormatter dateFormater = DateTimeFormatter.ofPattern("EEEE, MMM dd");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explorer);
+
+        lectures = ScheduleData.getLectures();//lecture data
+        container = findViewById(R.id.lecture_container);
+        prevButton = findViewById(R.id.btn_prev);
+        nextButton = findViewById(R.id.btn_next);
+        dayName = findViewById(R.id.explorer_day_name);
+        dayName.setText(days[index]);
+
+        Button LiveButton = findViewById(R.id.Live_button);
+        LiveButton.setOnClickListener(v ->{
+            finish();
+        });
+
+        for (int i = 0; i < days.length; i++) {
+            if(days[i].equals(today.toString()))
+                index = i;
+        }
+
+        dateHeader = findViewById(R.id.dateHeader);
+        dateHeader.setText(LocalDate.now().format(dateFormater));
+
+        if(prevButton != null)
+            prevButton.setOnClickListener(v -> {
+                if(index > 0) {
+                    index--;
+                    dayName.setText(days[index]);
+                    updateUI();
+                }
+            });
+
+        if(nextButton != null)
+            nextButton.setOnClickListener(v -> {
+                if(index < days.length - 1) {
+                    index++;
+                    dayName.setText(days[index]);
+                    updateUI();
+                }
+            });
+
+        updateUI();
+    }
+
+    public void updateUI() {
+        boolean foundAny = false; // if no course available no we will use it to display a message
+        container.removeAllViews();
+        LocalTime currentTime = LocalTime.now();
+        //today = LocalDate.now().getDayOfWeek();
+
+        dateHeader.setText(LocalDate.now().format(dateFormater));
+
+        LayoutInflater inflater = getLayoutInflater();
+        for (Lecture lecture : lectures) {
+            if (lecture.getDay().equalsIgnoreCase(days[index])) {
+                foundAny = true;
+                View lectureCard = inflater.inflate(R.layout.item_course, container, false);
+                TextView code = lectureCard.findViewById(R.id.lecture_code);
+                TextView name = lectureCard.findViewById(R.id.lecture_name);
+                TextView prof = lectureCard.findViewById(R.id.lecture_prof);
+                TextView section = lectureCard.findViewById(R.id.lecture_section);
+                TextView credit = lectureCard.findViewById(R.id.Lecture_credit);
+                TextView day = lectureCard.findViewById(R.id.lecture_day);
+                TextView time = lectureCard.findViewById(R.id.lecture_time);
+                TextView room = lectureCard.findViewById(R.id.lecture_room);
+
+                code.setText("Code: " + lecture.getCode());
+                name.setText("Course: " + lecture.getName());
+                prof.setText(lecture.getProf());
+                section.setText("Section: " + lecture.getSection());
+                credit.setText("Credit Hours: " + lecture.getCredit());
+                day.setText("Day: " + lecture.getDay());
+                time.setText(lecture.getStarttime().format(myFormat).toString() + " - " + lecture.getEndtime().format(myFormat).toString());
+                if (lecture instanceof Attend) {
+                    room.setText(((Attend) lecture).getRoom());
+                } else {
+                    room.setText("Online");
+                }
+
+                container.addView(lectureCard);
+            }
+        }
+        if (!foundAny) {
+            TextView emptyMessage = findViewById(R.id.empty);
+            emptyMessage.setVisibility(View.VISIBLE);
+        }else {
+            TextView emptyMessage = findViewById(R.id.empty);
+            emptyMessage.setVisibility(View.GONE);
+        }
+
+    }
+}
--- app/src/main/java/com/example/myschedule/MainActivity.java
+++ app/src/main/java/com/example/myschedule/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myschedule;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,7 +22,7 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity
 {
-    ArrayList<Lecture> lectures = new ArrayList<>();
+    public ArrayList<Lecture> lectures = new ArrayList<>();
     Handler handler = new Handler();
     Runnable runnable = new Runnable(){
         @Override
@@ -50,23 +52,15 @@ public class MainActivity extends AppCompatActivity
         container = findViewById(R.id.lecture_container);
         emptyMessage = findViewById(R.id.empty);
         dateHeader = findViewById(R.id.dateHeader);
+
+        Button fullButton = findViewById(R.id.full_button);
+        fullButton.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, ExplorerActivity.class);
+            startActivity(intent);
+        });
+
         // This connects the Java code to your XML layout
-
-
-        lectures.add(new Attend("Mth1402","Discrete Math", "Mohammed Alghamdi", "2", "3", "SUNDAY", LocalTime.of(10,0), LocalTime.of(11,50), "25-1.026"));
-        lectures.add(new Attend("SE1201","Foundations of Software Engineering", "Tariq Kashmeery", "4", "3", "SUNDAY", LocalTime.of(13,0), LocalTime.of(14,50), "25-1.025"));
-        lectures.add(new Attend("ELIE1202","English(1)", "Saleh Alghamdi", "12", "4", "MONDAY", LocalTime.of(8,0), LocalTime.of(11,50), "25-1.024"));
-        lectures.add(new Attend("SE1101","OOP Development", "Abdulhadi Al-Eidaroos", "2", "3", "MONDAY", LocalTime.of(13,0), LocalTime.of(14,50), "25-1.026"));
-        lectures.add(new Attend("MTH1181","Calculus", "Saber Mansour", "2", "4", "MONDAY", LocalTime.of(16,0), LocalTime.of(17,50), "15-1.006"));
-        lectures.add(new Attend("ELIE1202","English (3)(4)", "Saleh Alghamdi", "12", "4", "TUESDAY", LocalTime.of(13,0), LocalTime.of(17,50), "25-1.024"));
-        lectures.add(new Attend("SE1101","OOP Development(practical)", "Abdulhadi Al-Eidaroos", "2", "3", "TUESDAY", LocalTime.of(18,0), LocalTime.of(19,50), "12-1.016"));
-        lectures.add(new Online("QR2102","Memorize Quran", "Habiballah Alsulami", "64", "2", "WEDNESDAY", LocalTime.of(13,0), LocalTime.of(14,50)));
-        lectures.add(new Online("ELIE1202","English (2)", "Naseem Alotaibi", "12", "4", "WEDNESDAY", LocalTime.of(16,0), LocalTime.of(16,50)));
-        lectures.add(new Online("CUR1101","University Skills", "Fatimah Amrain", "188", "0", "WEDNESDAY", LocalTime.of(20,0), LocalTime.of(21,50)));
-        lectures.add(new Attend("MTH1402","Discrete Math (practical)", "Ismail Daghistani", "3", "3", "THURSDAY", LocalTime.of(8,0), LocalTime.of(9,50), "25-0.022"));
-        lectures.add(new Attend("MTH1181","Calculus", "Saber Mansour", "2", "4", "THURSDAY", LocalTime.of(10,0), LocalTime.of(11,50), "15-1.016"));
-        lectures.add(new Attend("SE1201","Foundation of Software Engineering", "Tariq Kashmeery", "4", "3", "THURSDAY",LocalTime.of(13,0), LocalTime.of(13,50), "25-1.025"));
-
+        lectures = ScheduleData.getLectures();
 
         handler.post(runnable);
         // Your test log
@@ -100,15 +94,15 @@ public class MainActivity extends AppCompatActivity
 
                     code.setText("Code: " + lecture.getCode());
                     name.setText("Course: " + lecture.getName());
-                    prof.setText("Professor: " + lecture.getProf());
+                    prof.setText(lecture.getProf());
                     section.setText("Section: " + lecture.getSection());
                     credit.setText("Credit Hours: " + lecture.getCredit());
                     day.setText("Day: " + lecture.getDay());
-                    time.setText("Time: " + lecture.getStarttime().format(myFormat).toString() + " - " + lecture.getEndtime().format(myFormat).toString());
+                    time.setText(lecture.getStarttime().format(myFormat).toString() + " - " + lecture.getEndtime().format(myFormat).toString());
                     if (lecture instanceof Attend) {
-                        room.setText(("Room: " + ((Attend) lecture).getRoom()));
+                        room.setText(((Attend) lecture).getRoom());
                     } else {
-                        room.setText("Room: Online");
+                        room.setText("Online");
                     }
 
                     container.addView(lectureCard);
